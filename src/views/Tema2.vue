6555839f68043dc5d53203d7274fba855a2083de
Luis Perez <ljpm9401@gmail.com>
2024-09-13 19:46:07 -0500

    Avances

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -11,6 +11,8 @@
       .col-lg-5.mb-4.mb-lg-0(data-aos="fade-up")
         .h-100.p-4.d-flex.align-content-center.flex-wrap(style="background-color:#CCF6F0;")
           p.mb-0 Las políticas empresariales no tienen el mismo impacto si los miembros de la empresa las desconocen. No basta con que sepan dónde encontrarlas; también es necesario que las comprendan. Es decir, cada empleado debe ser capaz de describir, con sus propias palabras, cómo su labor contribuye al cumplimiento de las políticas de la empresa. 
+      .col-lg-3.mb-4.mb-lg-0(data-aos="fade-down")
+        
 
 
 
